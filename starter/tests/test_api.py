b332c6c48bffb99ec64f59201eca794e3ab502d3
maherr13 <83926003+maherr13@users.noreply.github.com>
2023-11-22 21:31:58 +0200

    Add files via upload

---
+++ starter/tests/test_api.py
@@ -0,0 +1,57 @@
+"""
+Author: Ahmed Maher
+Date: Nov, 2023
+This script used for testing api
+"""
+
+import pytest
+from http import HTTPStatus
+from fastapi.testclient import TestClient
+from app.api import app
+
+client = TestClient(app)
+
+
+def assert_response(response, expected_status, expected_method):
+    assert response.status_code == expected_status
+    assert response.request.method == expected_method
+
+
+def test_greetings():
+    response = client.get('/')
+    assert_response(response, HTTPStatus.OK, "GET")
+    assert response.json() == "Hello PPL"
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ('age', "Age of the person - numerical - int"),
+    ('fnlgt', 'MORE INFO NEEDED - numerical - int'),
+    ('race', 'Race of the person - nominal categorical - str')
+])
+def test_feature_info(test_input: str, expected: str):
+    response = client.get(f'/feature_info/{test_input}')
+    assert_response(response, HTTPStatus.OK, "GET")
+    assert response.json() == expected
+
+
+def test_predict():
+    data = {
+        'age': 38,
+        'fnlwgt': 15,
+        'education-num': 1,
+        'capital-gain': 0,
+        'capital-loss': 0,
+        'hours-per-week': 5
+    }
+    response = client.post("/predict/", json=data)
+    assert_response(response, HTTPStatus.OK, "POST")
+    assert 0 <= response.json()['label'] <= 1
+    assert 0 <= response.json()['prob'] <= 1
+    assert response.json()['salary'] in ['>50k', '<=50k']
+
+
+def test_missing_feature_predict():
+    data = {"age": 0}
+    response = client.post("/predict/", json=data)
+    assert_response(response, HTTPStatus.UNPROCESSABLE_ENTITY, "POST")
+    assert response.json()["detail"][0]["type"] == "value_error.missing"
---
+++ starter/tests/test_types_ranges.py
@@ -0,0 +1,76 @@
+"""
+Author: Ahmed Maher
+Date: Nov, 2023
+This script used for testing types and ranges
+"""
+
+import great_expectations as ge
+
+
+def test_columns_exist(data: ge.dataset.PandasDataset):
+    expected_columns = [
+        'age',
+        'workclass',
+        'fnlwgt',
+        'education',
+        'educationnum',
+        'maritalstatus',
+        'occupation',
+        'relationship',
+        'race',
+        'sex',
+        'capitalgain',
+        'capitalloss',
+        'hoursperweek',
+        'nativecountry',
+        'salary']
+    for column in expected_columns:
+        assert data.expect_column_to_exist(
+            column)['success'], f"{column} does not exist"
+
+
+def test_column_dtypes(data: ge.dataset.PandasDataset):
+    expected_column_types = {
+        'age': 'int64',
+        'workclass': 'object',
+        'fnlwgt': 'int64',
+        'education': 'object',
+        'education-num': 'int64',
+        'marital-status': 'object',
+        'occupation': 'object',
+        'relationship': 'object',
+        'race': 'object',
+        'sex': 'object',
+        'capitalgain': 'int64',
+        'capitalloss': 'int64',
+        'hoursperweek': 'int64',
+        'nativecountry': 'object',
+        'salary': 'object'}
+    for column, dtype in expected_column_types.items():
+        assert data.expect_column_values_to_be_of_type(
+            column, dtype)['success'], f"{column} should be of type {dtype}"
+
+
+def test_education_num_column(data: ge.dataset.PandasDataset):
+    assert data.expect_column_values_to_be_between('education-num', 1, 17)[
+        'success'], "education_num column includes unknown category"
+
+
+def test_marital_status(data: ge.dataset.PandasDataset):
+    expected_categories = [
+        'never-married', 'married-civ-spouse', 'divorced',
+        'married-spouse-absent', 'separated', 'married-af-spouse', 'widowed'
+    ]
+    assert data.expect_column_distinct_values_to_equal_set('marital=status', expected_categories)[
+        'success'], "marital_status column includes unknown category"
+
+
+def test_label_salary(data: ge.dataset.PandasDataset):
+    expected_classes = ['<=50k', '>50k']
+    assert data.expect_column_distinct_values_to_equal_set('salary', expected_classes)[
+        'success'], "salary column includes more than two classes"
+
+
+def test_hours_per_week_range(data: ge.dataset.PandasDataset):
+    assert data.expect_column_values_to_be_between('hours-per-week', 1, 99)[
+        'success'], "hours_per_week column is not within range of 1 and 99"
