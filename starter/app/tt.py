65d77293e7a47ea539bd2af8479330e3af6de922
maherr13 <83926003+maherr13@users.noreply.github.com>
2023-11-22 20:14:22 +0200

    Update tt.py

--- starter/app/tt.py
+++ starter/app/tt.py
@@ -1,16 +1,26 @@
-import json 
 import requests
+import json
+
+
 
 data = {
         'age': 38,
-        'fnlgt': 15,
-        'education_num': 1,
-        'capital_gain': 0,
-        'capital_loss': 0,
-        'hours_per_week': 5
+        'workclass' : "state-gov",
+        'education' : "bachelors",
+        'marital_status' : "never-married",
+        'occupation' : "adm-clerical",
+        'relationship' : "not-in-family",
+        'race' : "white",
+        'sex' : "male",
+        'native-country' : "united-states",
+        'fnlwgt': 15,
+        'educationnum': 1,
+        'capitalgain': 0,
+        'capitalloss': 0,
+        'hoursperweek': 5,
+
     }
 
-
-r  = requests.post("http://127.0.0.1:8000/predict/", json = json.dumps(data))
+r = requests.post("http://127.0.0.1:8000/predict/", data=json.dumps(data))
 
 print(r.json())
