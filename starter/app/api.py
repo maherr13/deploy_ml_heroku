8b72eb51ca67a3a740cca075ef2ec635a007d7d0
maherr13 <83926003+maherr13@users.noreply.github.com>
2023-11-29 20:37:08 +0200

    Update api.py

--- starter/app/api.py
+++ starter/app/api.py
@@ -74,11 +74,11 @@ class Person(BaseModel):
     hoursperweek: int
     nativecountry: Optional[str] = None
 
-if "DYNO" in os.environ and os.path.isdir("../.dvc"):
-    os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r ../.dvc ../.apt/usr/lib/dvc")
+# if "DYNO" in os.environ and os.path.isdir("../.dvc"):
+#     os.system("dvc config core.no_scm true")
+#     if os.system("dvc pull") != 0:
+#         exit("dvc pull failed")
+#     os.system("rm -r ../.dvc ../.apt/usr/lib/dvc")
 
 app = FastAPI(
     title="Census",
